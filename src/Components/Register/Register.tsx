7b449b08512bc6a13773ffb61947542e02618e73
mingyeongho <1736s@naver.com>
2021-08-30 16:51:18 +0900

    redux + sessionStorage + Register

--- src/Components/Register/Register.tsx
+++ src/Components/Register/Register.tsx
@@ -1,25 +1,84 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import { store } from "../../App";
 import Interpreter from "../Interpreter/Interpreter";
 import "./Register.scss";
 
+interface Register {
+  username: string;
+  cellphone: string;
+  content_class: string;
+  content: string;
+  interpreter: number;
+  date: string;
+  time: string;
+}
+
 const Register = () => {
   // state
+  let cnt = 1;
+  const [register, setRegister] = useState<Register>({
+    username: JSON.parse(sessionStorage.getItem("user")!).user.userNickName,
+    cellphone: JSON.parse(sessionStorage.getItem("user")!).user.cellphone,
+    content_class: "",
+    content: "",
+    interpreter: 0,
+    date: "",
+    time: "",
+  });
+  const {
+    username,
+    cellphone,
+    content_class,
+    content,
+    interpreter,
+    date,
+    time,
+  } = register;
 
   // ref
-  const npc_wrapper = useRef<HTMLDivElement>(null);
+  const np_wrapper = useRef<HTMLDivElement>(null);
+  const cc_wrapper = useRef<HTMLDivElement>(null);
   const inter_wrapper = useRef<HTMLDivElement>(null);
   const dt_wrapper = useRef<HTMLDivElement>(null);
+  const nextBtn = useRef<HTMLButtonElement>(null);
+  const submitBtn = useRef<HTMLButtonElement>(null);
 
   // event
+  const onNextBtnClick = () => {
+    if (cnt === 1) {
+      np_wrapper.current!.style.display = "none";
+      cc_wrapper.current!.style.display = "block";
+    } else if (cnt === 2) {
+      cc_wrapper.current!.style.display = "none";
+      inter_wrapper.current!.style.display = "block";
+    } else if (cnt === 3) {
+      inter_wrapper.current!.style.display = "none";
+      dt_wrapper.current!.style.display = "block";
+      nextBtn.current!.style.display = "none";
+      submitBtn.current!.style.display = "block";
+    }
+    cnt += 1;
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(register);
+  };
 
   return (
-    <form className="register_wrapper">
+    <form className="register_wrapper" onSubmit={onSubmit}>
       <header className="register_header">SLT 수어 통역 신청</header>
-      <div className="name_phone_content_wrapper" ref={npc_wrapper}>
+      <div className="name_phone_wrapper" ref={np_wrapper}>
         <div className="name_wrapper">
           <label htmlFor="name">신청인</label>
           <div>
-            <input id="name" className="name input"></input>
+            <input
+              id="name"
+              className="name input"
+              autoComplete="off"
+              defaultValue={username}
+              readOnly
+            ></input>
           </div>
         </div>
         <div className="phone_wrapper">
@@ -29,14 +88,37 @@ const Register = () => {
               id="phone"
               className="phone input"
               placeholder="예) 010"
+              defaultValue={cellphone.substring(0, 3)}
+              readOnly
             ></input>
-            -<input className="phone input" placeholder="예) 1234"></input>-
-            <input className="phone input" placeholder="예) 1234"></input>
+            -
+            <input
+              className="phone input"
+              placeholder="예) 1234"
+              defaultValue={cellphone.substring(3, 7)}
+              readOnly
+            ></input>
+            -
+            <input
+              className="phone input"
+              placeholder="예) 1234"
+              defaultValue={cellphone.substring(7, 11)}
+              readOnly
+            ></input>
           </div>
-          <div className="content_wrapper">
-            <label htmlFor="content">내용</label>
-            <textarea rows={10} id="content"></textarea>
-          </div>
+        </div>
+      </div>
+      <div className="cc_wrapper" ref={cc_wrapper}>
+        <label htmlFor="content">내용</label>
+        <div className="content_class">
+          <label htmlFor="class">분류</label>
+          <select id="class">
+            <option defaultValue="medicine">의료</option>
+            <option defaultValue="edu">교육</option>
+          </select>
+        </div>
+        <div className="content_wrapper">
+          <textarea rows={10} id="content"></textarea>
         </div>
       </div>
       <div className="interpreters_wrapper" ref={inter_wrapper}>
@@ -54,13 +136,18 @@ const Register = () => {
         </div>
       </div>
       <div className="date_time_wrapper" ref={dt_wrapper}>
-        <div className="date"></div>
-        <div className="time"></div>
+        <div className="date">date</div>
+        <div className="time">time</div>
       </div>
-      <button type="button" className="next_btn btn">
+      <button
+        type="button"
+        className="next_btn btn"
+        ref={nextBtn}
+        onClick={onNextBtnClick}
+      >
         다음
       </button>
-      <button type="submit" className="submit_btn btn">
+      <button type="submit" className="submit_btn btn" ref={submitBtn}>
         신청
       </button>
     </form>
