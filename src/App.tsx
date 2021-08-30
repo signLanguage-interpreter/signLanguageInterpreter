53eefe310d6b765afc189b4c7721af0d9e8aaaab
mingyeongho <1736s@naver.com>
2021-08-30 16:52:24 +0900

    yarn add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { createStore } from "redux";
+
 import "./App.scss";
-import reducer from "./Components/Reducer";
+import reducer from "./Components/Reducer.js";
 import Router from "./Router";
 
 export const store = createStore(reducer);
+// const persistor = persistStore(store);
 
 function App() {
   return (
