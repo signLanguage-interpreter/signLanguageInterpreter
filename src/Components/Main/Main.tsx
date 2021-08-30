4aca59f284d7c2d613c5a400f2c7c6ced5128409
mingyeongho <1736s@naver.com>
2021-08-30 16:47:38 +0900

    redux + sessionStorage + Main

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,30 +1,58 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { RouteComponentProps } from "react-router-dom";
+import { store } from "../../App";
 import "./Main.scss";
 
 const Main: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
-  const session_login = sessionStorage.getItem("login");
+  // ref
   const login_text = useRef<HTMLButtonElement>(null);
 
+  // logout_action
+  const logout_action = (user: Record<string, string>, logged: Boolean) => {
+    return {
+      type: "LOGOUT",
+      payload: {
+        user: user,
+        logged: logged,
+      },
+    };
+  };
+
+  // event
   const onLoginClick = (e: any) => {
     let btn_txt = login_text.current!.innerText;
     if (btn_txt === "로그인") {
       history.push("/login");
     } else if (btn_txt === "로그아웃") {
       login_text.current!.innerText = "로그인";
-      sessionStorage.setItem("login", "false");
+      store.dispatch(
+        logout_action(
+          {
+            username: "",
+            password: "",
+            email: "",
+            userNickName: "",
+            cellphone: "",
+            birth: "",
+            gender: "",
+          },
+          false
+        )
+      );
     }
   };
 
   const onRegiClick = () => {
-    history.push("/register");
+    sessionStorage.getItem("user") !== null
+      ? history.push("/register")
+      : alert("로그인이 필요합니다.");
   };
 
   return (
     <main className="main">
       <h2>SLT</h2>
       <button className="link" onClick={onLoginClick} ref={login_text}>
-        {session_login === "true" ? "로그아웃" : "로그인"}
+        {sessionStorage.getItem("user") !== null ? "로그아웃" : "로그인"}
       </button>
       <button className="link" onClick={onRegiClick}>
         신청
