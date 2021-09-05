5e1ad1238c1282cc6c3b8202d9d3150f2429837d
mingyeongho <1736s@naver.com>
2021-09-05 16:25:38 +0900

    remove use strict

--- src/Components/Reducer.js
+++ src/Components/Reducer.js
@@ -1,31 +1,45 @@
-"use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
+var __assign =
+  (this && this.__assign) ||
+  function () {
+    __assign =
+      Object.assign ||
+      function (t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
+          s = arguments[i];
+          for (var p in s)
+            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
         }
         return t;
-    };
+      };
     return __assign.apply(this, arguments);
-};
+  };
 exports.__esModule = true;
 var initialState = {
-    authorization: null,
-    logged: false
+  authorization: null,
+  logged: false,
 };
 var reducer = function (state, action) {
-    if (state === void 0) { state = initialState; }
-    switch (action.type) {
-        case "LOGIN":
-            sessionStorage.setItem("authorization", JSON.stringify(action.payload.authorization));
-            return __assign(__assign({}, state), { authorization: action.payload.authorization, logged: action.payload.logged });
-        case "LOGOUT":
-            sessionStorage.clear();
-            return __assign(__assign({}, state), { authorization: null, logged: false });
-        default:
-            return state;
-    }
+  if (state === void 0) {
+    state = initialState;
+  }
+  switch (action.type) {
+    case "LOGIN":
+      sessionStorage.setItem(
+        "authorization",
+        JSON.stringify(action.payload.authorization)
+      );
+      return __assign(__assign({}, state), {
+        authorization: action.payload.authorization,
+        logged: action.payload.logged,
+      });
+    case "LOGOUT":
+      sessionStorage.clear();
+      return __assign(__assign({}, state), {
+        authorization: null,
+        logged: false,
+      });
+    default:
+      return state;
+  }
 };
 exports["default"] = reducer;
