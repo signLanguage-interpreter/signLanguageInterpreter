83ea7c4dabf7e15a4ccfa20587af146f166b52a9
mingyeongho <1736s@naver.com>
2021-09-07 01:00:23 +0900

    remove register

--- src/Router.js
+++ src/Router.js
@@ -4,7 +4,7 @@ import loadable from "@loadable/component";
 const Main = loadable(() => import("./Components/Main/Main"));
 const Login = loadable(() => import("./Components/Login/Login"));
 const Join = loadable(() => import("./Components/Join/Join"));
-const Register = loadable(() => import("./Components/Register/Register"));
+
 const Manager = loadable(() => import("./Components/Manager/Manager"));
 const Modify = loadable(() => import("./Components/Modify/Modify"));
 
@@ -14,7 +14,6 @@ const Router = () => {
       <Route exact path="/user/main" component={Main} />
       <Route exact path={["/", "/login"]} component={Login} />
       <Route exact path="/join" component={Join} />
-      <Route exact path="/register" component={Register} />
       <Route exact path="/manager" component={Manager} />
       <Route exact path="/modifyInfo" component={Modify} />
     </Switch>
