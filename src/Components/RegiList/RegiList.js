b3f7b84399a87b16a70e07745e2d1c473521e339
mingyeongho <1736s@naver.com>
2021-09-07 15:02:51 +0900

    add Dummy data

---
+++ src/Components/RegiList/RegiList.js
@@ -0,0 +1,21 @@
+import "./RegiList.scss";
+
+const RegiList = () => {
+  return (
+    <div className="regi_lsit_wrapper">
+      <h4>자신이 신청한 리스트</h4>
+      <div className="regi_list">
+        <span>#</span>
+        <span>Subject</span>
+        <span>Date</span>
+        <span>Compelete</span>
+        <span>1</span>
+        <span>신청합니다.</span>
+        <span>210907</span>
+        <span>X</span>
+      </div>
+    </div>
+  );
+};
+
+export default RegiList;
