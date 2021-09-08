3da71f949ac6f97b74ae6a099cbd0b51ee6fab95
mingyeongho <1736s@naver.com>
2021-09-08 11:06:52 +0900

    add props

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import logo from "../img/logo.png";
+import Register from "../Register/Register";
 import MyContent from "../MyContent/MyContent";
 import RegiList from "../RegiList/RegiList";
-import Register from "../Register/Register";
 import "./Main.scss";
 
 const Main = () => {
+  const [user, setUser] = useState({
+    id: "",
+    userNickName: "",
+    cellPhone: "",
+    eMail: "",
+  });
+  const [regi_list, setRegi_list] = useState([]);
   // useEffect
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/user/main", {
+          headers: {
+            Authorization: sessionStorage
+              .getItem("authorization")
+              ?.substring(
+                1,
+                sessionStorage.getItem("authorization").length - 1
+              ),
+          },
+        });
+        setUser(res.data.user);
+        setRegi_list(res.data.regi_list);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetch();
+  });
+
   return (
     <div className="main">
       <header>
@@ -22,10 +53,10 @@ const Main = () => {
           <Register></Register>
         </section>
         <section className="user">
-          <MyContent></MyContent>
+          <MyContent user={user}></MyContent>
         </section>
         <section className="regi_list">
-          <RegiList></RegiList>
+          <RegiList regi_list={regi_list}></RegiList>
         </section>
       </main>
     </div>
