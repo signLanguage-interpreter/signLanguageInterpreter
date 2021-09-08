02d09cc0eb0af10c034eeacc9a25814c2caef9b0
mingyeongho <1736s@naver.com>
2021-09-08 11:07:10 +0900

    add map function

--- src/Components/RegiList/RegiList.js
+++ src/Components/RegiList/RegiList.js
@@ -1,18 +1,22 @@
 import "./RegiList.scss";
 
-const RegiList = () => {
+const RegiList = ({ regi_list }) => {
   return (
     <div className="regi_lsit_wrapper">
       <h4>자신이 신청한 리스트</h4>
       <div className="regi_list">
         <span>#</span>
-        <span>Subject</span>
-        <span>Date</span>
-        <span>Compelete</span>
-        <span>1</span>
-        <span>신청합니다.</span>
-        <span>210907</span>
-        <span>X</span>
+        <span>제목</span>
+        <span>예약날짜</span>
+        <span>상태</span>
+        {regi_list.map((cur) => {
+          <>
+            <span>{cur.id}</span>
+            <span>{cur.subject}</span>
+            <span>{cur.receptionDate}</span>
+            <span>{cur.status === "HOLD" ? "X" : "O"}</span>
+          </>;
+        })}
       </div>
     </div>
   );
