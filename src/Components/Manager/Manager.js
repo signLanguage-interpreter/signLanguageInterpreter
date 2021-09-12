1d07c3d850d5f92eb281b7b55eacbc932c5476b9
mingyeongho <1736s@naver.com>
2021-09-12 20:31:37 +0900

    add import

--- src/Components/Manager/Manager.js
+++ src/Components/Manager/Manager.js
@@ -52,7 +52,9 @@ const Manager = ({ history }) => {
             onClick={() => window.location.replace("/manager/main")}
           ></img>
           <div>
-            <span>다른 통역사 접수 보러가기</span>
+            <span onClick={() => history.push("/manager/all_register")}>
+              다른 통역사 접수 보러가기
+            </span>
             <span onClick={() => history.push("/user/main")}>
               신청하러 가기
             </span>
