8d86927b5da6c54362648f642b197ff143b31dab
mingyeongho <1736s@naver.com>
2021-09-14 17:53:37 +0900

    모든 신청 리스트(HOLD)

--- src/Components/Manager/AllList/AllList.js
+++ src/Components/Manager/AllList/AllList.js
@@ -1,59 +1,34 @@
 import axios from "axios";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import "./AllList.scss";
 
-const AllList = ({ registers, id, history }) => {
-  const regi_bool = registers === null ? false : true;
-  let regi_res;
+const AllList = () => {
+  const [lists, setLists] = useState([]);
 
-  const onClick = () => {
-    const send = async () => {
+  useEffect(() => {
+    const fetch = async () => {
       try {
-        await axios.post("http://localhost:5000", {
-          headers: {
-            Authorization: sessionStorage
-              .getItem("authorization")
-              ?.substring(
-                1,
-                sessionStorage.getItem("authorization").length - 1
-              ),
-          },
-        });
-        window.location.replace("/manager/main");
+        const res = await axios.get(
+          "http://localhost:5000/manager?status=HOLD&page=1",
+          {
+            headers: {
+              Authorization: sessionStorage
+                .getItem("authorization")
+                ?.substring(
+                  1,
+                  sessionStorage.getItem("authorization").length - 1
+                ),
+            },
+          }
+        );
+        setLists(res.data);
       } catch (e) {
         console.error(e);
       }
     };
-    send();
-  };
+    fetch();
+  });
 
-  if (regi_bool) {
-    regi_res = registers
-      .filter((register) => register.status === "HOLD")
-      .map((cur, idx) => {
-        const date = cur.receptionDate
-          .substring(2, 16)
-          .replaceAll("-", ".")
-          .replace("T", " ");
-        return (
-          <Fragment key={idx}>
-            <span
-              className="regi_subject"
-              onClick={() =>
-                history.push(`/user/regist/${id}/${cur.receptionId}`)
-              }
-            >
-              {cur.subject}
-            </span>
-            <span>{date}</span>
-            <span>H</span>
-            <span>
-              <button onClick={onClick}>접수</button>
-            </span>
-          </Fragment>
-        );
-      });
-  }
   return (
     <div className="all_list_wrapper">
       <h4>모든 신청 리스트</h4>
@@ -62,8 +37,8 @@ const AllList = ({ registers, id, history }) => {
         <span>예약날짜</span>
         <span>상태</span>
         <span>접수</span>
-        {regi_res}
       </div>
+      <div className="pagination">pagination</div>
     </div>
   );
 };
