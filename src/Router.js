bfad3cd52476673948b3f8b04695a1ea1922e078
mingyeongho <1736s@naver.com>
2021-09-14 17:57:41 +0900

    modify Router and add qs

--- src/Router.js
+++ src/Router.js
@@ -9,6 +9,17 @@ const Modify = loadable(() => import("./Components/Modify/Modify"));
 const RegiBoard = loadable(() => import("./Components/RegiBoard/RegiBoard"));
 const AllRegi = loadable(() => import("./Components/All_Regi/AllRegi"));
 const Nomatch = loadable(() => import("./Components/Nomatch/Nomatch"));
+const ManagerModify = loadable(() =>
+  import("./Components/ManagerModify/ManagerModify")
+);
+const ManagerSignUp = loadable(() =>
+  import("./Components/ManagerSignUp/ManagerSignUp")
+);
+const AllList = loadable(() => import("./Components/Manager/AllList/AllList"));
+const MyList = loadable(() => import("./Components/Manager/MyList/MyList"));
+const CompleteList = loadable(() =>
+  import("./Components/Manager/CompleteList/CompleteList")
+);
 
 const Router = () => {
   return (
@@ -18,9 +29,17 @@ const Router = () => {
       {/* user */}
       <Route exact path="/user/main" component={Main} />
       <Route exact path="/user/modifyMember/:id" component={Modify} />
+      <Route exact path="/user/managerSignUp" component={ManagerSignUp} />
       {/* manager */}
       <Route exact path="/manager/main" component={Manager} />
       <Route exact path="/manager/all_register" component={AllRegi} />
+      <Route exact path="/manager/register" component={ManagerModify} />
+      {/* 모든 신청 리스트 */}
+      <Route exact path="/manager/all_list" component={AllList} />
+      {/* 자신에게 온 신청 리스트 */}
+      <Route exact path="/manager/my_list" component={MyList} />
+      {/* 완료된 신청 리스트 */}
+      <Route exact path="/manager/complete_list" component={CompleteList} />
       {/* register */}
       <Route exact path="/user/regist/:pk/:receptionId" component={RegiBoard} />
       <Route component={Nomatch} />
