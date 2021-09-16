d4a51117090a35106298ea6425c80f9434759904
mingyeongho <1736s@naver.com>
2021-09-16 22:45:31 +0900

    list & pagination

--- src/Components/Manager/CompleteList/CompleteList.js
+++ src/Components/Manager/CompleteList/CompleteList.js
@@ -2,14 +2,16 @@ import axios from "axios";
 import { Fragment, useEffect, useState } from "react";
 import "./CompleteList.scss";
 
-const CompleteList = ({ id, history }) => {
+const CompleteList = ({ history, match }) => {
+  const cur_page = match.params.page;
+  const [page, setPage] = useState({});
   const [lists, setLists] = useState([]);
 
   useEffect(() => {
     const fetch = async () => {
       try {
         const res = await axios.get(
-          "http://localhost:5000/manager?status=END&page=1",
+          `http://localhost:5000/manager?status=END&page=${cur_page}`,
           {
             headers: {
               Authorization: sessionStorage
@@ -21,14 +23,24 @@ const CompleteList = ({ id, history }) => {
             },
           }
         );
-        console.log(res);
-        setLists(res.data);
+        setPage(res.data.paging);
+        setLists(res.data.lists);
       } catch (e) {
         console.error(e);
       }
     };
     fetch();
   });
+
+  // conditional rendering
+  const pagination = () => {
+    const res = [];
+    for (let i = page.startPage; i <= page.endPage; i++) {
+      res.push(<span className="page">{i}</span>);
+    }
+  };
+
+  const onBtnClick = () => {};
   return (
     <div className="com_list_wrapper">
       <h4>완료된 신청 리스트</h4>
@@ -37,8 +49,26 @@ const CompleteList = ({ id, history }) => {
         <span>예약날짜</span>
         <span>상태</span>
         <span>접수</span>
+        {lists === null
+          ? null
+          : lists.map((cur) => {
+              return (
+                <Fragment key="cur.id">
+                  <span>{cur.subject}</span>
+                  <span
+                    onClick={() =>
+                      history.push(`/user/regist/${cur.id}/${cur.receptionId}`)
+                    }
+                  >
+                    {cur.receptionDate}
+                  </span>
+                  <span>{cur.status}</span>
+                  <button onClick={onBtnClick}>접수</button>
+                </Fragment>
+              );
+            })}
       </div>
-      <div className="pagination">pagination</div>
+      <div className="pagination">{pagination()}</div>
     </div>
   );
 };
